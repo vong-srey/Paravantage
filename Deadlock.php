f7cf7d941b1e181b833e2cbcbb5e32a52ab595b5
nancywatta <nancy.watta@gmail.com>
2014-04-30 22:36:35 +1200

    Final Changes

--- Deadlock.php
+++ Deadlock.php
@@ -71,7 +71,8 @@
 		this.ok = function() {
 			document.getElementById('dialogbox').style.display = "none";
 			document.getElementById('dialogoverlay').style.display = "none";	
-			document.getElementById("analogy").style.visibility = "visible";		
+			document.getElementById("analogy").style.visibility = "visible";
+			document.getElementById("analogy2").style.visibility = "visible";
 		}
 	}
 	var Alert = new CustomAlert();
@@ -115,15 +116,21 @@
 				arr = a.split("|");
 				document.userForm.question.value = arr[0];
 				document.vpForm.question.value = arr[2];
-				document.getElementById("lockingText").style.visibility = "visible";
 				document.userForm.submit.disabled=false;
+				document.getElementById("lockbutton").style.visibility = "visible";
 				document.getElementById("Q1").style.visibility = "hidden";
-				document.getElementById("Q4").style.visibility = "hidden";
+				document.getElementById("Q3").style.visibility = "hidden";
+				document.getElementById("L1").style.visibility = "hidden";
+				document.getElementById("L3").style.visibility = "hidden";
+				document.load.loadButton.disabled=true;
+				document.getElementById("BobQ1").style.visibility = "visible";
+				document.getElementById("VPQ3").style.visibility = "visible";
+				document.getElementById("text1").style.backgroundColor="#00FF00"
+				document.getElementById("text4").style.backgroundColor="#00FF00"
 				document.getElementById("lockImage2").style.visibility = "visible";
 				document.getElementById("lockImage1").style.visibility = "visible";
-				document.load.loadButton.disabled=true;
-				document.getElementById("text1").style.backgroundColor="#00FF00"
-				document.getElementById("text2").style.backgroundColor="#00FF00"
+				document.getElementById("lockbutton").style.visibility = "visible";
+				document.getElementById("lockbutton2").style.visibility = "visible";
 			}
 		}
 
@@ -131,9 +138,16 @@
 
 	var i=1;
 	function nextQuest(){
-		if( i==1 ) {
-			document.userForm.question.value = arr[i];
-			document.getElementById("lockingText"+i).style.visibility = "visible";
+			document.getElementById("Bob"+i).style.backgroundColor="#00FF00"
+			if(i==1)
+				document.getElementById("VP" +i).style.backgroundColor="#00FF00"
+			document.userForm.submit.disabled=true;
+			document.userForm.lockbutton.disabled=false;
+			i=i+1;
+	}
+	function nextLock(){
+		if( i==2 ) {
+			document.userForm.question.value = arr[i-1];
 			document.vpForm.answer.value = "";
 			document.vpForm.question.value = "";
 			document.vpForm.question.style.backgroundImage = "url('data/wait1.png')";
@@ -141,15 +155,17 @@
 			document.vpForm.question.style.backgroundSize="50px 50px"
 			document.vpForm.question.style.backgroundPosition="top center"
 			document.userForm.answer.value="";
+			document.getElementById("Bob"+i).style.backgroundColor="#00FF00"
+			document.getElementById("VP" +i).style.backgroundColor="#FF0000"
 			document.getElementById("Q2").style.visibility = "hidden";
-			document.getElementById("text3").style.backgroundColor="#00FF00"
-			document.getElementById("text4").style.backgroundColor="#00FF00"
-			document.getElementById("text1").style.backgroundColor="initial"
-			document.getElementById("text2").style.backgroundColor="initial"
+			document.getElementById("L2").style.visibility = "hidden";
+			document.getElementById("BobQ2").style.visibility = "visible";
+			document.userForm.submit.disabled=false;
+			document.userForm.lockbutton.disabled=true;
+			document.getElementById("lockImage3").style.visibility = "visible";
 			i=i+1;
 		}
 		else {
-			document.getElementById("lockingText4").style.visibility = "visible";
 			document.userForm.answer.value = "";
 			document.userForm.question.value = "";
 			document.userForm.question.style.backgroundImage = "url('data/wait1.png')";
@@ -158,8 +174,8 @@
 			document.userForm.question.style.backgroundPosition="top center"
  			document.userForm.submit.disabled="disabled";
 			document.load.loadButton.disabled="disabled";
-			document.getElementById("text3").style.backgroundColor="initial"
-			document.getElementById("text5").style.backgroundColor="#00FF00"
+			document.userForm.lockbutton.disabled=true;
+			document.getElementById("Bob"+i).style.backgroundColor="#FF0000"
 			var theDelay = 8;
   			var timer = setTimeout("Freeze()",theDelay*1000)
 		}
@@ -167,9 +183,12 @@
 
 	function Freeze(){
 		var dialogoverlay = document.getElementById('dialogoverlay');
+         dialogoverlay.innerHTML = '<img src="data/deadlock.png" width="500" height="500" title="Lock" alt="Lock" align="center" onclick="unfreeze()" />';
 		dialogoverlay.style.display = "block";
 		dialogoverlay.style.height = winH+"px";
-		dialogoverlay.innerHTML = '<img src="data/deadlock.png" width="20" height="20" title="Lock" alt="Lock" align="right" />';
+	}
+	function unfreeze(){
+		document.getElementById('dialogoverlay').style.display = "none";
 	}
 
 	</script>
@@ -191,116 +210,129 @@
 		<h1>
 			<center>Parallel Programming Pitfalls</center>
 		</h1>
-		<h2>
-			<center>Deadlock</center>
-		</h2>
+		<h2><center>Deadlock</center></h2> 
 		<div align="right">
 			<a href="index.php" style="color: #CC0000"><right> HOME </right></a>
 		</div>
+			
 	</div>
 	<!-- @ Header ends here +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 
 	<div id="content">
-		<div id="left">
-			<div id="resources" ><center>
-				Resources:
-				<button id="Q1" style="background-color:#E96D63; color:#7FCA9F">Q1</button>
-				<button id="Q2" style="background-color:#E96D63; color:#7FCA9F">Q2</button>
-				<button id="Q3" style="background-color:#E96D63; color:#7FCA9F">Q3</button>
+	<div id="resources" style="font-family: Comic Sans MS; color: #E96D63;font-size: 12pt;"><center>
+				Shared &nbsp;&nbsp; Resources:&nbsp;&nbsp;
+				<span id="Q1" style="color:#7FCA9F">Q1</span>&nbsp;&nbsp;
+				<span id="Q2" style="color:#7FCA9F">Q2</span>&nbsp;&nbsp;
+				<span id="Q3" style="color:#7FCA9F">Q3</span>&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;&nbsp;
+				Locks:&nbsp;&nbsp;
+				<span id="L1" style="color:#7FCA9F">L1</span>&nbsp;&nbsp;
+				<span id="L2" style="color:#7FCA9F">L2</span>&nbsp;&nbsp;
+				<span id="L3" style="color:#7FCA9F">L3</span>
 				</center>
 			</div>
-			<br />
-			<h3>Bob (Process 1)</h3>
-			<br /> 
-			<h3>QUESTION:</h3>
+		<div id="left">
+			
+			<h3>Bob (Thread 1)</h3>
+			 <br/>
+			<h3>QUESTION:   &nbsp;&nbsp;&nbsp;&nbsp;             
+				<span id="BobQ1" style="visibility: hidden;color:#7FCA9F">Q1</span>
+				<span id="BobQ2" style="visibility: hidden;color:#7FCA9F"> , Q2</span>
+			</h3>
 			<br />
 			<form name="userForm">
 				<div id="p1-question">
 					<input type="text" name="question"
-						style="width: 300px; height: 50px;font-family: Comic Sans MS;font-size: 15pt;" readonly="readonly"></input>
+						style="width: 300px; height: 50px;font-family: Comic Sans MS;font-size: 15pt;color: blue; background-color:rgb(136, 162, 168)" readonly="readonly"></input>
 				</div>
 				<br /> <br /> <input type="text" name="answer"
 					style="width: 200px; height: 30px;"></input> <br /> <br /> <input
 					type="button" onclick="nextQuest()" value="Submit" name="submit" disabled="disabled" style="font-size:30pt;"> </input>
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<input type="button" id="lockbutton" disabled="disabled" name="lockbutton" value="Lock Next Question" onclick="nextLock()" style="visibility: hidden;font-size:30pt;"> </input>
 			</form>
-			<br /> <br /> <br /> <br />
+			<br /> <br /> <br />
 			<div id="load-game">
 				<form name="load">
-					<input type="button" onclick="loadGame()" value="Load Game"
+					<input type="button" onclick="loadGame()" value="Load Game & Lock The First Question"
 						name="loadButton" style="font-size:30pt;"> </input>
 				</form>
 			</div>
 			<br/>
 			<img id="lockImage1" src="data/Lock.jpg" width="80" height="80" title="Lock"
-				alt="Lock" align="right" style="visibility: hidden;"/><br /> <br />
-
+				alt="Lock" align="left" style="visibility: hidden;"/><img id="lockImage3" src="data/Lock.jpg" width="80" height="80" title="Lock"
+				alt="Lock" align="left" style="visibility: hidden;"/><br /> <br /><br/><br /><br /><br />
 		</div>
 
 		<div id="middle">
-			<h3>Stack Traces</h3>
-			<div id="bobText" style="visibility: hidden; color: #FFCC00">
-				<h3>--------->  Bob Enters</h3>
-			</div>
-			<div id="compText" style="visibility: hidden; color: Yellow">
-				<h3>&nbsp;&nbsp;---------> Virtual Player Enters</h3>
-			</div>
-			<div id="lockingText" style="visibility: hidden; color: Green">
-				<h3 style="color: Green">--------->  Bob locks Question1</h3>
-				<h3 style="color: Yellow">&nbsp;&nbsp;---------> VP locks Question3</h3>
-			</div>
-			<div id="lockingText1" style="visibility: hidden; color: Yellow">
-				<h3 style="color: Yellow">--------->  Bob locks Question2</h3>
-				<p style="color: Green">&nbsp;&nbsp;---------> VP waits to acquire lock on Question1</p>
-			</div>
-			<div id="lockingText4" style="visibility: hidden; color: Yellow">
-				<h3 style="color: Yellow">--------->  Bob is waiting to acquire lock on Question3
-					held by VP</h3>
-				<h3 style="color: Green">&nbsp;&nbsp;---------> VP is waiting to acquire lock on Question1
-					which is held by Bob</h3>
-			</div>
-			<h3>
+			<br/><br /> <br /> <br /> <br />
+			<br /> <br /> <br /> <br />
+			<br /> <br />
+			<h2>
 			<div id="analogy" style="color: #FF9900; visibility: hidden;">
-				<span style="float: left">Process1() { </span> <span style="float: right">Process2() { &nbsp;&nbsp;&nbsp;&nbsp;</span>
-				<br/><span id="text1" style="float: left">&nbsp;&nbsp;	Lock Q1;</span> <span id="text2" style="float: right">&nbsp;&nbsp;	Lock Q3;</span>
-				<br/><span id="text3" style="float: left">&nbsp;&nbsp;	Lock Q2;</span> <span id="text4" style="float: right">&nbsp;&nbsp;	Lock Q1;</span>
-				<br/><span id="text5" style="float: left">&nbsp;&nbsp;	Lock Q3;</span> <span style="float: right">&nbsp;&nbsp;	Lock Q2;</span>
-				<br/><span style="float: left">&nbsp;&nbsp;	UnLock Q1;</span> <span style="float: right">&nbsp;&nbsp;	UnLock Q3;</span>
-				<br/><span style="float: left">&nbsp;&nbsp;	UnLock Q2;</span> <span style="float: right">&nbsp;&nbsp;	UnLock Q1;</span>
-				<br/><span style="float: left">&nbsp;&nbsp;	UnLock Q3;</span> <span style="float: right">&nbsp;&nbsp;	UnLock Q2;</span>
-				<br/><span style="float: left">} </span> <span style="float: right"> }&nbsp;&nbsp;&nbsp;&nbsp; </span>
-			</div></h3>
+				<span style="float: left">Thread1() { </span>
+				<br/><span id="text1" style="float: left">&nbsp;&nbsp;	Lock Q1;</span>
+				<br/><span id="Bob1" style="float: left">&nbsp;&nbsp;	Answer Q1;</span>
+				<br/><span id="Bob2" style="float: left">&nbsp;&nbsp;	Lock Q2;</span>
+				<br/><span id="Bob3" style="float: left">&nbsp;&nbsp;	Answer Q2;</span>
+				<br/><span id="Bob4" style="float: left">&nbsp;&nbsp;	Lock Q3;</span> 
+				<br/><span id="text3" style="float: left">&nbsp;&nbsp;	Answer Q3;</span>
+				<br/><span style="float: left">&nbsp;&nbsp;	UnLock Q1;</span> 
+				<br/><span style="float: left">&nbsp;&nbsp;	UnLock Q2;</span> 
+				<br/><span style="float: left">&nbsp;&nbsp;	UnLock Q3;</span>
+				<br/><span style="float: left">} </span>
+			</div></h2>
+			<br />
+		</div>
+			
+		<div id="middleleft">
+			<br/><br /> <br /> <br /> <br />
+			<br /> <br /> <br /> <br />
+			<br /> <br />
+			<h2>
+			<div id="analogy2" style="color: #FF9900; visibility: hidden;">
+				 <span style="float: left">Thread2() { &nbsp;&nbsp;&nbsp;&nbsp;</span>
+				 <br/><span id="text4" style="float: left">&nbsp;&nbsp;	Lock Q3;</span>
+				 <br/><span id="VP1" style="float: left">&nbsp;&nbsp;	Answer Q3;</span>
+				 <br/><span id="VP2" style="float: left">&nbsp;&nbsp;	Lock Q1;</span>
+				 <br/><span style="float: left">&nbsp;&nbsp;	Answer Q1;</span>
+				 <br/><span style="float: left">&nbsp;&nbsp;	Lock Q2;</span>
+				 <br/><span style="float: left">&nbsp;&nbsp;	Answer Q2;</span>
+				 <br/><span style="float: left">&nbsp;&nbsp;	UnLock Q3;</span>
+				 <br/><span style="float: left">&nbsp;&nbsp;	UnLock Q1;</span>
+				 <br/><span style="float: left">&nbsp;&nbsp;	UnLock Q2;</span>
+				<br/><span style="float: left"> }&nbsp;&nbsp;&nbsp;&nbsp; </span>
+			</div></h2>
 			<br/>
 		</div>
+		
 
 		<div id="right">
-			<div id="resources2"><center>
-			Resources:
-				<button id="Q4" style="background-color:#E96D63; color:#7FCA9F"3>Q3</button>
-				<button id="Q5" style="background-color:#E96D63; color:#7FCA9F" >Q1</button>
-				<button id="Q6" style="background-color:#E96D63; color:#7FCA9F">Q2</button>
-				</center>
-			</div>
-			<br />
-			<h3>Virtual Player (VP) (Process 2)</h3>
-			<br /> 
-			<h3>QUESTION:</h3>
+			
+			<h3>Virtual Player (VP) (Thread 2)</h3>
+			 <br/>
+			<h3>QUESTION:&nbsp;&nbsp;&nbsp;&nbsp;             
+				<span id="VPQ3" style="visibility: hidden;color:#7FCA9F">Q3</span>
+			</h3>
 			<br />
 			<form name="vpForm">
 				<input type="text" name="question"
-					style="width: 300px; height: 50px;font-family: Comic Sans MS;font-size: 15pt;" readonly="readonly"></input> <br />
+					style="width: 300px; height: 50px;font-family: Comic Sans MS;font-size: 15pt;color: blue; background-color:rgb(100, 140, 140);" readonly="readonly"></input> <br />
 				<br /> <br /> <input type="text" name="answer"
 					style="width: 200px; height: 30px;" readonly="readonly"></input> <br />
 				<br /> <input type="button" value="Submit" disabled="disabled" style="font-size:30pt; font-family: Comic Sans MS;"> </input>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<button id="lockbutton2" disabled="disabled" style="visibility: hidden; font-size:10pt; font-family: Comic Sans MS;"> Lock Next Question</BUTTON>
 			</form>
-			<br /> <br /> <br /> <br />
+			<br /> <br /> <br />
 			<div id="load-game">
 				<form>
-					<input type="button" onclick="loadGame()" value="Load Game"
+					<input type="button" onclick="loadGame()" value="Load Game & Lock The First Question"
 						disabled="disabled" style="font-size:30pt; font-family: 'Comic Sans MS';"> </input>
 				</form>
 			</div><br/>
 			<img id="lockImage2" src="data/Lock.jpg" width="80" height="80" title="Lock"
-				alt="Lock" align="left" style="visibility: hidden;"/> <br /> <br /> <br /> <br />
+				alt="Lock" align="left" style="visibility: hidden;"/> <br /> <br /> <br /> <br /><br /><br />
 		</div>
 	</div>
 
